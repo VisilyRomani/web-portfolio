7f79d9b6e18effad0b13c1be93d6d5ecc87fd7d9
visily <michael4161@hotmail.com>
2023-09-12 20:22:31 -0600

    init routing layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	const toggle = (e: Event) => {
+		const html = document.getElementById('html-theme');
+		if (html) {
+			if ((e.target as HTMLInputElement).checked) {
+				html.setAttribute('data-theme', 'dark');
+			} else {
+				html.setAttribute('data-theme', 'light');
+			}
+		}
+	};
+</script>
+
+<nav style="display:flex; align-items:center; margin:1em">
+	<h1 style="padding:0; margin:0">Michael Wong</h1>
+	<div style="display: flex; gap:1em;">
+		<a href="/projects">PROJECTS</a>
+		<a href="/about">RESUME</a>
+		<a href="/about">ABOUT</a>
+		<a href="/contact">CONTACT</a>
+		<input type="checkbox" on:change={toggle} />
+	</div>
+</nav>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+	const toggle = () => {
+		const html = document.getElementById('html-theme');
+		if (html) {
+			if (html?.attributes.getNamedItem('data-theme')?.value === 'light') {
+				html?.setAttribute('data-theme', 'dark');
+			} else {
+				html?.setAttribute('data-theme', 'light');
+			}
+		}
+	};
+</script>
+
+<button on:click={toggle}>Theme</button>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
