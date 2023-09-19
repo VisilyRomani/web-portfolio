b5449f0a28fc98a606e4c627da0cf089cb63bf61
visily <michael4161@hotmail.com>
2023-09-18 18:54:53 -0600

    starting menu, fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,90 @@
 <script lang="ts">
-	const toggle = (e: Event) => {
-		const html = document.getElementById('html-theme');
-		if (html) {
-			if ((e.target as HTMLInputElement).checked) {
-				html.setAttribute('data-theme', 'dark');
-			} else {
-				html.setAttribute('data-theme', 'light');
-			}
-		}
-	};
+	import '../main.css';
+
+	// unplugin-icons bug https://github.com/unplugin/unplugin-icons/issues/308
+
+	//@ts-ignore
+	import BurgerIcon from '~icons/quill/hamburger';
+
+	let mobileToggle = false;
 </script>
 
-<nav style="display:flex; align-items:center; margin:1em">
-	<h1 style="padding:0; margin:0">Michael Wong</h1>
-	<div style="display: flex; gap:1em;">
-		<a href="/projects">PROJECTS</a>
-		<a href="/about">RESUME</a>
-		<a href="/about">ABOUT</a>
-		<a href="/contact">CONTACT</a>
-		<input type="checkbox" on:change={toggle} />
-	</div>
+<nav>
+	<a href="/">
+		<h1>Michael Wong</h1>
+	</a>
+	<ul class="nav-container">
+		<!-- <BurgerIcon style="font-size: 3em" /> -->
+
+		<li>
+			<a class="link" href="/projects">PROJECTS</a>
+		</li>
+		<li>
+			<a class="link" href="/about">RESUME</a>
+		</li>
+		<li>
+			<a class="link" href="/about">ABOUT</a>
+		</li>
+		<li>
+			<a class="link" href="/contact">CONTACT</a>
+		</li>
+	</ul>
 </nav>
 
 <slot />
+
+<style>
+	@font-face {
+		font-family: 'HelloEngineer';
+		src: url('/fonts/HelloEngineer.ttf');
+	}
+	@font-face {
+		font-family: 'PWScratched';
+		src: url('/fonts/PWScratchedfont.ttf');
+	}
+
+	.nav-container {
+		display: flex;
+		width: 100%;
+		flex-direction: column;
+		justify-content: end;
+		gap: 1em;
+		list-style-type: none;
+	}
+
+	@media (min-width: 700px) {
+		.nav-container {
+			flex-direction: row;
+		}
+	}
+	li {
+		display: flex;
+		align-items: center;
+	}
+	.link {
+		font-family: 'HelloEngineer';
+		text-align: center;
+		border: 1px solid;
+		padding: 0.2em;
+	}
+	a {
+		color: white;
+		font-size: xx-large;
+		text-decoration: none;
+	}
+	h1 {
+		text-align: center;
+		padding: 0;
+		font-size: 40px;
+		font-family: 'PWScratched';
+		margin: 0;
+		margin-top: auto;
+		margin-bottom: auto;
+	}
+	nav {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		padding: 1em;
+	}
+</style>
